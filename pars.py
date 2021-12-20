179bd8ccaa1a08a0f0216f51385f4b48d0348e4b
uebermensch1488 <74329180+uebermensch1488@users.noreply.github.com>
2021-12-21 00:02:50 +0300

    Working sel

--- pars.py
+++ pars.py
@@ -4,96 +4,50 @@ import time
 from random import randint
 import os
 import re
+from selenium import webdriver
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
 
 def parse(links, name, available_nums):
     REGION_INFO = 'region_info'
     CATEGORIES_INFO = 'categories_info'
     AVITO_MAIN = 'avito_main'
     #available_nums = int(balance/11)
-    proxy_flag = 0
     sessids = file_to_array('sessid.txt')
-    proxies = file_to_array('proxies.txt')
     sessid = str(sessids[-1])
-    cookie = f'sessid={sessid};'
     numbers_flag = 0
-    s = requests.Session()                          # Будем всё делать в рамках одной сессии
-    headers = { 'authority': 'm.avito.ru',
-                'pragma': 'no-cache',
-                'cache-control': 'no-cache',
-                'upgrade-insecure-requests': '1',
-                'user-agent': 'Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.66 Mobile Safari/537.36',
-                'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
-                'sec-fetch-site': 'none',
-                'sec-fetch-mode': 'navigate',
-                'sec-fetch-user': '?1',
-                'sec-fetch-dest': 'document',
-                'accept-language': 'ru-RU,ru;q=0.9',}
-    if cookie:                                      # Добавим куки, если есть внешние куки
-        headers['cookie'] = cookie
-    s.headers.update(headers)                       # Сохраняем заголовки в сессию
+
+    cookie = {"name":"sessid", "value":sessid}
+
+    options = webdriver.FirefoxOptions()
+    options.set_preference("general.useragent.override", "Mozilla/95.0.2 (iPhone; U; CPU like Mac OS X; en) AppleWebKit/420+ (KHTML, like Gecko) Version/3.0 Mobile/1A543 Safari/419.3")
+    options.set_preference('permissions.default.image', 2)
+    options.set_preference('dom.ipc.plugins.enabled.libflashplayer.so', 'false')
+    options.add_argument('--no-sandbox')
+    options.headless = True
+    driver = webdriver.Firefox(options=options)
+    driver.get("https://m.avito.ru")
+    driver.add_cookie(cookie)
     nums = []
     links = links[:available_nums+1]
 
     for link in links:
-        time.sleep(5)
-        print(link)
-        add_id = int(link.split("_")[-1])
         try:
-            num = s.get(f'https://m.avito.ru/api/1/items/{add_id}/phone?key=af0deccbgcgidddjgnvljitntccdduijhdinfgjgfjir').json()['result']['action']['uri']
-            if num[-11:]!="uthenticate":
-                nums.append(num[-11:])
-            else:
-                while num[-11:]=="uthenticate":
-                    delete_last_line('sessid.txt')
-                    sessid = str(sessids[-1])
-                    cookie = f'sessid={sessid};'
-                    s = requests.Session()                          # Будем всё делать в рамках одной сессии
-                    headers = { 'authority': 'm.avito.ru',
-                                'pragma': 'no-cache',
-                                'cache-control': 'no-cache',
-                                'upgrade-insecure-requests': '1',
-                                'user-agent': 'Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.66 Mobile Safari/537.36',
-                                'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
-                                'sec-fetch-site': 'none',
-                                'sec-fetch-mode': 'navigate',
-                                'sec-fetch-user': '?1',
-                                'sec-fetch-dest': 'document',
-                                'accept-language': 'ru-RU,ru;q=0.9',}
-                    if cookie:                                      # Добавим куки, если есть внешние куки
-                        headers['cookie'] = cookie
-                    s.headers.update(headers)                       # Сохраняем заголовки в сессию
-                    num = s.get(f'https://m.avito.ru/api/1/items/{add_id}/phone?key=af0deccbgcgidddjgnvljitntccdduijhdinfgjgfjir').json()['result']['action']['uri']
-                nums.append(num[-11:])
-        
-        except Exception:
-            try:
-                s.get(f'https://m.avito.ru/api/1/items/{add_id}/phone?key=af0deccbgcgidddjgnvljitntccdduijhdinfgjgfjir').json()['error']
-                delete_last_line('sessid.txt')
-                sessid = str(sessids[-1])
-                cookie = f'sessid={sessid};'
-                s = requests.Session()                          # Будем всё делать в рамках одной сессии
-                headers = { 'authority': 'm.avito.ru',
-                            'pragma': 'no-cache',
-                            'cache-control': 'no-cache',
-                            'upgrade-insecure-requests': '1',
-                            'user-agent': 'Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.66 Mobile Safari/537.36',
-                            'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
-                            'sec-fetch-site': 'none',
-                            'sec-fetch-mode': 'navigate',
-                            'sec-fetch-user': '?1',
-                            'sec-fetch-dest': 'document',
-                            'accept-language': 'ru-RU,ru;q=0.9',}
-                if cookie:                                      # Добавим куки, если есть внешние куки
-                    headers['cookie'] = cookie
-                s.headers.update(headers)                       # Сохраняем заголовки в сессию
-                num = s.get(f'https://m.avito.ru/api/1/items/{add_id}/phone?key=af0deccbgcgidddjgnvljitntccdduijhdinfgjgfjir').json()['result']['action']['uri']
-                nums.append(num)
-            except Exception:
-                nums.append("-")
+            driver.get(link)
+            #WebDriverWait(driver,10).until(EC.element_to_be_clickable(By.XPATH, "//button[@class='mav-vpko6w']")).click()
+            WebDriverWait(driver, 20).until(EC.presence_of_element_located((By.XPATH, "//button[@class='mav-vpko6w']"))).click()
+            time.sleep(1)
+            nums.append(driver.find_element_by_xpath("//span[@data-marker='phone-popup/phone-number']").text)
 
-        for num in nums:
-            if num != "-":
-                numbers_flag+=1
+        except Exception as e:
+            print(e)
+            nums.append("-")
+
+    for num in nums:
+        if num != "-":
+            numbers_flag+=1
+
     #delete_last_line('sessid.txt')
     numbers_flag = array_to_file(nums, name, numbers_flag)
     return numbers_flag
